dc853f2f5cbbe3c3162101ab6bf762561220b5c0
ZurdoAsd <gaspar.ajs@gmail.com>
2022-09-15 20:33:36 -0300

    mod

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./Card.module.css";
 import { Link } from "react-router-dom";
+import notFound from "./404.png"
 
 
 export default function Card({ id, name, sprite, types }) {
@@ -10,9 +11,8 @@ export default function Card({ id, name, sprite, types }) {
           <h2>{name}</h2>
           <h4>TYPE: {types?.map((e) => e.toUpperCase() + " ")} </h4>
         </div>
-
         <Link to={`/home/${id}`}>
-        <img src={sprite} alt="img" className={s.Img}/>
+        <img src={sprite.length? sprite : notFound} alt="img" className={s.Img}/>
         </Link>
       </ul>
   );
--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -39,9 +39,6 @@ export default function Home() {
     setShowsearch(false);
   };
 
- 
-
-
   return (
     <div className={s.container}>
        <div className={s.navbar}>
